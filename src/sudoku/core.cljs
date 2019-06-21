f4c032bd0602cc91bca25e456eaa301781e653a7
Bobby Towers <porkostomus@mail.com>
2019-06-21 11:12:32 -0700

    UI fixes

--- src/sudoku/core.cljs
+++ src/sudoku/core.cljs
@@ -7,11 +7,9 @@
 
 (defn multiply [a b] (* a b))
 
-(def grid-size 9)
-
 (defn positions []
-  (for [i (range grid-size)
-        j (range grid-size)]
+  (for [i (range 9)
+        j (range 9)]
     [j i]))
 
 (def partial-puzzle
@@ -25,25 +23,40 @@
    [6 7 5 0 4 2 8 3 1]
    [8 9 2 1 6 3 5 7 4]])
 
+(def sudoku-001
+  {:puzzle [0 0 4 3 0 0 2 0 9 0 0 5 0 0 9 0 0 1 0 7 0 0 6 0 0 4 3 0 0 6 0 0 2 0 8 7 1 9 0 0
+            0 7 4 0 0 0 5 0 0 8 3 0 0 0 6 0 0 0 0 0 1 0 5 0 0 3 5 0 8 6 9 0 0 4 2 9 1 0 3 0 0]
+   :answer [8 6 4 3 7 1 2 5 9 3 2 5 8 4 9 7 6 1 9 7 1 2 6 5 8 4 3 4 3 6 1 9 2 5 8 7 1 9 8 6 5
+            7 4 3 2 2 5 7 4 8 3 9 1 6 6 8 9 7 3 4 1 2 5 7 1 3 5 2 8 6 9 4 5 4 2 9 1 6 3 7 8]})
+
+(def sudoku-002
+  {:puzzle [0 4 0 1 0 0 0 5 0 1 0 7 0 0 3 9 6 0 5 2 0 0 0 8 0 0 0 0 0 0 0 0 0 0 1 7 0 0 0 9 0 6
+            8 0 0 8 0 3 0 5 0 6 2 0 0 9 0 0 6 0 5 4 3 6 0 0 0 8 0 7 0 0 2 5 0 0 9 7 1 0 0]
+   :answer [3 4 6 1 7 9 2 5 8 1 8 7 5 2 3 9 6 4 5 2 9 6 4 8 3 7 1 9 6 5 8 3 2 4 1 7 4 7 2 9
+            1 6 8 3 5 8 1 3 7 5 4 6 2 9 7 9 8 2 6 1 5 4 3 6 3 1 4 8 5 7 9 2 2 5 4 3 9 7 1 8 6]})
+
 (def puzzle [0 0 4 3 0 0 2 0 9 0 0 5 0 0 9 0 0 1 0 7 0 0 6 0 0 4 3 0 0 6 0 0 2 0 8 7 1 9 0 0 0 7 4 0 0 0 5 0 0 8 3 0 0 0 6 0 0 0 0 0 1 0 5 0 0 3 5 0 8 6 9 0 0 4 2 9 1 0 3 0 0])
 (def answer [8 6 4 3 7 1 2 5 9 3 2 5 8 4 9 7 6 1 9 7 1 2 6 5 8 4 3 4 3 6 1 9 2 5 8 7 1 9 8 6 5 7 4 3 2 2 5 7 4 8 3 9 1 6 6 8 9 7 3 4 1 2 5 7 1 3 5 2 8 6 9 4 5 4 2 9 1 6 3 7 8])
+
 (defn init-matrix []
   (into {}
         (map vector
              (positions)
-             (flatten puzzle))))
+             puzzle)))
 
 (def board (atom (init-matrix)))
 
-(def cell-input (atom nil))
-
-(def val-input (atom nil))
+(def cell-input (atom "(Click square to select)"))
 
 (defn rect-cell [[x y]]
   [:rect.cell
-   {:x (+ 0.05 x) :width 0.9
-    :y (+ 0.05 y) :height 0.9
-    :fill "white"
+   {:x (+ 0 x) :width 1
+    :y (+ 0 y) :height 1
+    :fill (cond
+            (= [x y] @cell-input) "violet"
+            (and (< 2 x 6) (< 2 y 6)) "lightpink"
+            (or (< 2 x 6) (< 2 y 6)) "cyan"
+            :else "lightpink")
     :stroke-width 0.025
     :stroke "black"
     :on-click
@@ -62,7 +75,7 @@
 (defn render-board [app-state]
   (into
    [:svg.board
-    {:view-box (str "0 0 " grid-size " " grid-size)
+    {:view-box "0 0 9 9"
      :shape-rendering "auto"
      :style {:max-height "500px"}}]
    (for [[pos condition] app-state]
@@ -73,14 +86,13 @@
 (defn input-field []
   (fn []
     [:div
-     [:p "Enter value: "
       [:input {:type "number"
-               :value (get @cell-input @board)
-               :on-change #(swap! board assoc @cell-input (js/parseInt (-> % .-target .-value)))}]]]))
+               :min 0 :max 9
+               :value (get @board @cell-input)
+               :on-change #(swap! board assoc @cell-input (js/parseInt (-> % .-target .-value)))}]]))
 
 (defn attempt [state]
-  (into [] (for [i (range grid-size)
-                 j (range grid-size)]
+  (into [] (for [i (range 9) j (range 9)]
              (get state [j i]))))
 
 (defn win? [state]
@@ -89,13 +101,11 @@
 
 (defn sudoku []
   [:center
-   [:p (str "Enter for cell " @cell-input)]
+   [:h3 "Click a square to change its value"]
    [input-field]
+   [:br]
    [:div [render-board @board]]
-   [:p (str "Win? = " (win? @board))]
-   [:p (str "Because " (attempt @board) (if (win? @board)
-                                          " equals "
-                                          " does not equal ") answer)]])
+   [:h3 (str "Win? = " (win? @board))]])
 
 ;; define your app data so that it doesn't get over-written on reload
 
